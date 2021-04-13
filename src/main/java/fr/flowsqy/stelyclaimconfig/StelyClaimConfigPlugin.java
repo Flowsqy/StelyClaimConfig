874fbb020897d10df07c93e7fda47dd438974787
Flowsqy <Flo504.Dev@gmail.com>
2021-04-13 22:30:40 +0200

    Add configuration

--- src/main/java/fr/flowsqy/stelyclaimconfig/StelyClaimConfigPlugin.java
+++ src/main/java/fr/flowsqy/stelyclaimconfig/StelyClaimConfigPlugin.java
@@ -1,6 +1,51 @@
 package fr.flowsqy.stelyclaimconfig;
 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class StelyClaimConfigPlugin extends JavaPlugin {
+
+    private YamlConfiguration configuration;
+
+    @Override
+    public void onEnable() {
+        final Logger logger = getLogger();
+        final File dataFolder = getDataFolder();
+
+        if (!checkDataFolder(dataFolder)) {
+            logger.log(Level.WARNING, "Can not write in the directory : " + dataFolder.getAbsolutePath());
+            logger.log(Level.WARNING, "Disable the plugin");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        this.configuration = initFile(dataFolder, "config.yml");
+    }
+
+    private boolean checkDataFolder(File dataFolder) {
+        if (dataFolder.exists())
+            return dataFolder.canWrite();
+        return dataFolder.mkdirs();
+    }
+
+    private YamlConfiguration initFile(File dataFolder, String fileName) {
+        final File file = new File(dataFolder, fileName);
+        if (!file.exists()) {
+            try {
+                Files.copy(Objects.requireNonNull(getResource(fileName)), file.toPath());
+            } catch (IOException ignored) {
+            }
+        }
+
+        return YamlConfiguration.loadConfiguration(file);
+    }
+
 }
