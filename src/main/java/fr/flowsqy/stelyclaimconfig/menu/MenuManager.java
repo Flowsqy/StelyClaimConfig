1ff2cf0c6698153ba26f4c4275eedc4498ddd79f
Flowsqy <Flo504.Dev@gmail.com>
2021-04-17 12:43:46 +0200

    Add list of sessions id for region sessions

--- src/main/java/fr/flowsqy/stelyclaimconfig/menu/MenuManager.java
+++ src/main/java/fr/flowsqy/stelyclaimconfig/menu/MenuManager.java
@@ -3,29 +3,34 @@ package fr.flowsqy.stelyclaimconfig.menu;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import fr.flowsqy.abstractmenu.factory.MenuFactory;
 import fr.flowsqy.abstractmenu.inventory.EventInventory;
+import fr.flowsqy.abstractmenu.item.CreatorListener;
 import fr.flowsqy.abstractmenu.item.ItemBuilder;
 import fr.flowsqy.stelyclaim.io.Messages;
 import fr.flowsqy.stelyclaimconfig.StelyClaimConfigPlugin;
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemFlag;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MenuManager {
 
     private final Messages messages;
     private final Map<String, PlayerSession> playerSessions;
+    private final Map<String, List<String>> regionPlayers;
     private final List<Integer> slots;
     private final EventInventory inventory;
 
     public MenuManager(StelyClaimConfigPlugin plugin, YamlConfiguration menuConfiguration) {
         messages = plugin.getMessages();
         playerSessions = new HashMap<>();
+        regionPlayers = new HashMap<>();
         slots = new ArrayList<>(0);
         final MenuFactory factory = new MenuFactory(plugin);
         final ConfigurationSection section = menuConfiguration.getConfigurationSection("menu");
@@ -42,6 +47,14 @@ public class MenuManager {
 
     public void open(Player player, ProtectedRegion region) {
         inventory.open(player, player.getName());
+        playerSessions.put(player.getName(), new PlayerSession(calculateFlags(player), region.getId(), 1));
+        final List<String> inventories = regionPlayers.computeIfAbsent(region.getId(), k -> new ArrayList<>());
+        inventories.add(player.getName());
+    }
+
+    private List<String> calculateFlags(Player player) {
+        // TODO Fill with allowed flags
+        return new ArrayList<>();
     }
 
     private void applySession(InventoryClickEvent event) {
@@ -49,21 +62,44 @@ public class MenuManager {
     }
 
     private void close(Player player) {
-        playerSessions.remove(player.getName());
+        final PlayerSession session = playerSessions.remove(player.getName());
+        if (session != null) {
+            final List<String> players = regionPlayers.get(session.getSessionId());
+            players.remove(player.getName());
+            if (players.size() == 0) {
+                regionPlayers.remove(session.getSessionId());
+            }
+        }
+    }
+
+    private void handleFlagClick(InventoryClickEvent event) {
+
     }
 
     private final static class PlayerSession {
 
         private final List<String> flags;
+        private final String sessionId;
+        private final Map<String, Boolean> changes;
         private int page;
 
-        public PlayerSession(List<String> flags, int page) {
+        public PlayerSession(List<String> flags, String sessionId, int page) {
             this.flags = flags;
+            this.sessionId = sessionId;
+            this.changes = new HashMap<>();
             this.page = page;
         }
 
         public List<String> getFlags() {
             return flags;
+        }
+
+        public String getSessionId() {
+            return sessionId;
+        }
+
+        public Map<String, Boolean> getChanges() {
+            return changes;
         }
 
         public int getPage() {
@@ -72,6 +108,49 @@ public class MenuManager {
 
         public void setPage(int page) {
             this.page = page;
+        }
+    }
+
+    private final class FlagCreatorListener implements CreatorListener {
+
+        @Override
+        public String handleName(String name) {
+            return null;
+        }
+
+        @Override
+        public List<String> handleLore(List<String> lore) {
+            return null;
+        }
+
+        @Override
+        public boolean handleUnbreakable(boolean unbreakable) {
+            return false;
+        }
+
+        @Override
+        public Material handleMaterial(Material material) {
+            return null;
+        }
+
+        @Override
+        public int handleAmount(int amount) {
+            return 0;
+        }
+
+        @Override
+        public Map<Enchantment, Integer> handleEnchants(Map<Enchantment, Integer> enchants) {
+            return null;
+        }
+
+        @Override
+        public Set<ItemFlag> handleFlags(Set<ItemFlag> flags) {
+            return null;
+        }
+
+        @Override
+        public Map<Attribute, AttributeModifier> handleAttributes(Map<Attribute, AttributeModifier> attributes) {
+            return null;
         }
     }
 
@@ -98,14 +177,15 @@ public class MenuManager {
                     );
                     break;
                 case "flags":
+                    builder.creatorListener(new FlagCreatorListener());
                     eventInventory.register(
                             builder,
-                            e -> {
-                            },// TODO Store somewhere the changes
+                            MenuManager.this::handleFlagClick,
                             slots
                     );
                     MenuManager.this.slots.addAll(slots);
                     break;
+                case "info":
                 default:
                     eventInventory.register(
                             builder,
